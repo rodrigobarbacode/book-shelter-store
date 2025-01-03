e5be14b3dcd3a6a5fac3db749c565b02bbb415cd
luisr_7l55t6z <luisr_7l55t6z@THINKCENTRE>
2025-01-02 23:18:03 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace book_shelter_store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
